b664385ca51f26b770786d2033df975d0c50b2b7
Mariah Wall <mgwall17@gmail.com>
2016-11-01 13:27:08 -0700

    modified:   index.html 	modified:   menutab.js 	modified:   style.css 	modified:   tabstyle.css Shrunk icons, fixed wrapper inconsistencies, updated button formats and fixed the javascript for the tab menu, fixed captialization and reorganized product cards.

--- menutab.js
+++ menutab.js
@@ -9,6 +9,8 @@ function openTab(evt, tabName) {
         tabContent[i].style.display = "none";
     }
 
+    //
+
     //Grab elements with "tabLinks" and remove "active" class
     tabLinks = document.getElementsByClassName("tabLinks");
     for (i = 0; i < tabLinks.length; i++) {
@@ -16,7 +18,7 @@ function openTab(evt, tabName) {
     }
 
     //Show current tab, and add "active" class to the link that opened the tab
-    
+
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
